19cc3d364c70fd5a5b1299eaa34b3ec01502b4b9
xiong <807062493@qq.com>
2020-09-09 17:24:58 +0800

    会动的

--- cv/dist/style.e308ff8e.js
+++ cv/dist/style.e308ff8e.js
@@ -117,37 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"main.js":[function(require,module,exports) {
-var style = document.querySelector('#style');
-var html = document.querySelector('#html');
-var n = 0;
-var string2 = "";
-var string = "\n    /*\u753B\u4E00\u4E2ADIV~*/\n\n    #div1 {\n        width:200px;\n        height:200px;\n        border:1px solid red;\n    }\n\n    /*\u628ADIV\u53D8\u6210\u5706\u5F62*/\n\n    #div1{\n        border-radius:50%;\n        box-shadow: 0 0 3px rgba(0,0,0,0.5);\n        border: none;\n    }\n\n    /*\u7528\u6E10\u53D8\u5206\u6210\u9ED1\u767D\u4E24\u534A*/\n\n    #div1{\n        background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);\n    }\n\n    /* \u52A0\u4E24\u4E2A\u5706*/\n\n    #div1::before{\n        content: \"\";\n        display: block;\n        position: absolute;\n        width: 100px;\n        height: 100px;\n        transform: translateX(50%);\n        background: #000;\n        border-radius: 50%;\n        background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);\n    }\n    \n    #div1::after{\n        content: \"\";\n        display: block;\n        position: absolute;\n        width: 100px;\n        height: 100px;\n        transform: translate(50%,100%);\n        background: #fff;\n        border-radius: 50%;\n        background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);\n    }\n\n    /*\u9F20\u6807\u653E\u5728\u516B\u5366\u4E0A\uFF0C\u516B\u5366\u53EF\u4EE5\u65CB\u8F6C\u54E6~*/\n\n   \n    \n    \n";
+})({"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-var step = function step() {
-  setTimeout(function () {
-    if (string[n] === '\n') {
-      string2 += '<br>';
-    } else if (string[n] === ' ') {
-      string2 += '&nbsp';
-    } else {
-      string2 += string[n];
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
     }
 
-    html.innerHTML = string2;
-    style.innerHTML = string.substring(0, n);
-    window.scrollTo(0, 99999);
-    html.scrollTo(0, 99999);
+    cssTimeout = null;
+  }, 50);
+}
 
-    if (n < string.length - 1) {
-      n++;
-      step();
-    }
-  }, 0);
-};
+module.exports = reloadCSS;
+},{"./bundle-url":"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/bundle-url.js"}],"style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
 
-step();
-},{}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/css-loader.js"}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -351,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/style.e308ff8e.js.map
--- cv/src/main.js
+++ cv/src/main.js
@@ -1,6 +1,82 @@
-console.log("hi")
-let demo = document.querySelector('#demo');
-let n = 0;
-demo.innerHTML = n;
+let style = document.querySelector('#style');
+let html = document.querySelector('#html');
 
-setInterval(() => { n = n + 1; demo.innerHTML = n; }, 500)
+let n = 0;
+let string2 = "";
+let string = `
+    /*画一个DIV~*/
+
+    #div1 {
+        width:200px;
+        height:200px;
+        border:1px solid red;
+    }
+
+    /*把DIV变成圆形*/
+
+    #div1{
+        border-radius:50%;
+        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        border: none;
+    }
+
+    /*用渐变分成黑白两半*/
+
+    #div1{
+        background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+    }
+
+    /* 加两个圆*/
+
+    #div1::before{
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        transform: translateX(50%);
+        background: #000;
+        border-radius: 50%;
+        background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+    }
+    
+    #div1::after{
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        transform: translate(50%,100%);
+        background: #fff;
+        border-radius: 50%;
+        background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+    }
+
+    /*鼠标放在八卦上，八卦可以旋转哦~*/
+
+   
+    
+    
+`
+let step = () => {
+    setTimeout(() => {
+        if (string[n] === '\n') {
+            string2 += '<br>';
+        } else if (string[n] === ' ') {
+            string2 += '&nbsp';
+        } else {
+            string2 += string[n];
+        }
+
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 99999);
+        if (n < string.length - 1) {
+            n++;
+            step();
+        }
+    }, 0);
+}
+
+step();
